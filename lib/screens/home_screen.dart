4834b0e7e77f1017145f5a43beba1b5b2b0fd713
Shadowstorm <khanisorn.sr@ku.th>
2024-02-20 23:31:48 +0700

    create login screen but there are some bug

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,9 +4,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,19 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_project/models/user.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _username = '';
+  String _password = '';
+  String _error = '';
+
+  final _formKey = GlobalKey<FormState>();
+
+  void _Login() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      User user = User(_username, _password, '', '', '', '');
+      if (user.validatePassword(_password)) {
+        Navigator.pushNamed(context, '/home');
+      } else {
+        setState(() {
+          _error = 'Invalid username or password';
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/home');
-          },
-          child: Text('Login'),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image.asset(
+              'assets/images/login-page-image.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Text(
+                      'Login',
+                      style: GoogleFonts.gurajada(
+                        fontSize: 40.0,
+                      ),
+                    ),
+                  ]),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
+                    onSaved: (value) {
+                      _username = value!;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a username';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    onSaved: (value) {
+                      _password = value!;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: _Login,
+                    child: const Text('Login'),
+                  ),
+                  Text(
+                    _error,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ]),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
