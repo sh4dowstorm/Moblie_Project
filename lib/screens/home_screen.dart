5668e0bd1d36d5f5f3c325203d979a1337b5d6c0
INV8 <chaiyapat.sr@ku.th>
2024-03-19 19:14:44 +0700

    delete unnecessary line

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:mobile_project/models/location.dart';
 import 'package:mobile_project/screens/place_detail_screen.dart';
 import 'package:mobile_project/widgets/place_display.dart';
--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_gradient_app_bar/flutter_gradient_app_bar.dart';
 import 'package:mobile_project/models/location.dart';
 
