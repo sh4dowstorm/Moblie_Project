57cedd9ced96eb0f3d7c1af063a75fc9b3cdf612
INV8 <chaiyapat.sr@ku.th>
2024-03-28 22:25:27 +0700

    delete unnecessary line

--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobile_project/services/firebase_loader.dart';
